76fb07397065a6bb087584b0c6265276b88cbf6d
Jacob Schwartz <schwja04@gmail.com>
2025-01-13 22:22:11 -0600

    Update naming

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 ### Resource Group - Begin ###
-resource "azurerm_resource_group" "tf-practice-rg" {
+resource "azurerm_resource_group" "developer-rg" {
   name     = "${var.resource_group_config.prefix}-rg"
   location = var.resource_group_config.location
 
@@ -8,28 +8,28 @@ resource "azurerm_resource_group" "tf-practice-rg" {
 ### Resource Group - End ###
 
 ### Network - Begin ###
-resource "azurerm_virtual_network" "tf-practice-vnet" {
+resource "azurerm_virtual_network" "developer-vnet" {
   name                = "${var.resource_group_config.prefix}-vnet"
-  resource_group_name = azurerm_resource_group.tf-practice-rg.name
-  location            = azurerm_resource_group.tf-practice-rg.location
+  resource_group_name = azurerm_resource_group.developer-rg.name
+  location            = azurerm_resource_group.developer-rg.location
   address_space       = var.vnet_address_space
 
   tags = var.resource_group_config.tags
 }
 
-resource "azurerm_subnet" "tf-practice-subnet" {
+resource "azurerm_subnet" "developer-subnet" {
   name                 = "${var.resource_group_config.prefix}-subnet"
-  resource_group_name  = azurerm_resource_group.tf-practice-rg.name
-  virtual_network_name = azurerm_virtual_network.tf-practice-vnet.name
+  resource_group_name  = azurerm_resource_group.developer-rg.name
+  virtual_network_name = azurerm_virtual_network.developer-vnet.name
   address_prefixes     = [for each in var.vnet_address_space: cidrsubnet(each, 8, 2)]
 }
 ### Network - End ###
 
 ### Network Security Group - Begin ###
-resource "azurerm_network_security_group" "tf-practice-sg" {
+resource "azurerm_network_security_group" "developer-sg" {
   name                = "${var.resource_group_config.prefix}-sg"
-  location            = azurerm_resource_group.tf-practice-rg.location
-  resource_group_name = azurerm_resource_group.tf-practice-rg.name
+  location            = azurerm_resource_group.developer-rg.location
+  resource_group_name = azurerm_resource_group.developer-rg.name
 
   tags = var.resource_group_config.tags
 }
@@ -44,21 +44,21 @@ resource "azurerm_network_security_rule" "tf-practice-developer-rule" {
   destination_port_range      = "*"
   source_address_prefixes     = length(var.vm_config.accessing_ips) > 0 ? var.vm_config.accessing_ips : ["*"] 
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.tf-practice-rg.name
-  network_security_group_name = azurerm_network_security_group.tf-practice-sg.name
+  resource_group_name         = azurerm_resource_group.developer-rg.name
+  network_security_group_name = azurerm_network_security_group.developer-sg.name
 }
 
-resource "azurerm_subnet_network_security_group_association" "tf-practice-sg-association" {
-  subnet_id                 = azurerm_subnet.tf-practice-subnet.id
-  network_security_group_id = azurerm_network_security_group.tf-practice-sg.id
+resource "azurerm_subnet_network_security_group_association" "developer-sg-association" {
+  subnet_id                 = azurerm_subnet.developer-subnet.id
+  network_security_group_id = azurerm_network_security_group.developer-sg.id
 }
 ### Network Security Group - End ###
 
 ### Public IP - Begin ###
-resource "azurerm_public_ip" "tf-practice-public-ip" {
+resource "azurerm_public_ip" "developer-public-ip" {
   name                = "${var.resource_group_config.prefix}-public-ip"
-  location            = azurerm_resource_group.tf-practice-rg.location
-  resource_group_name = azurerm_resource_group.tf-practice-rg.name
+  location            = azurerm_resource_group.developer-rg.location
+  resource_group_name = azurerm_resource_group.developer-rg.name
   allocation_method   = "Dynamic"
   sku                 = "Basic"
 
@@ -68,16 +68,16 @@ resource "azurerm_public_ip" "tf-practice-public-ip" {
 
 ### Virtual Machine - Resources - Begin ###
 #### Virtual Machine NIC - Begin ####
-resource "azurerm_network_interface" "tf-practice-nic" {
+resource "azurerm_network_interface" "developer-nic" {
   name                = "${var.resource_group_config.prefix}-nic"
-  location            = azurerm_resource_group.tf-practice-rg.location
-  resource_group_name = azurerm_resource_group.tf-practice-rg.name
+  location            = azurerm_resource_group.developer-rg.location
+  resource_group_name = azurerm_resource_group.developer-rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.tf-practice-subnet.id
+    subnet_id                     = azurerm_subnet.developer-subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.tf-practice-public-ip.id
+    public_ip_address_id          = azurerm_public_ip.developer-public-ip.id
   }
 
   tags = var.resource_group_config.tags
@@ -105,8 +105,8 @@ data "cloudinit_config" "vm-init" {
 #### Cloud Init - End ####
 
 #### Source Image Reference - Begin ####
-data "azurerm_platform_image" "tf-practice-image" {
-  location  = azurerm_resource_group.tf-practice-rg.location
+data "azurerm_platform_image" "developer-vm-image" {
+  location  = azurerm_resource_group.developer-rg.location
   publisher = var.vm_config.os_image.publisher
   offer     = var.vm_config.os_image.offer
   sku       = var.vm_config.os_image.sku
@@ -115,13 +115,13 @@ data "azurerm_platform_image" "tf-practice-image" {
 #### Source Image Reference - End ####
 
 #### Virtual Machine - Begin ####
-resource "azurerm_linux_virtual_machine" "tf-practice-vm" {
+resource "azurerm_linux_virtual_machine" "developer-vm" {
   name                  = "${var.resource_group_config.prefix}-linux-vm"
-  location              = azurerm_resource_group.tf-practice-rg.location
-  resource_group_name   = azurerm_resource_group.tf-practice-rg.name
+  location              = azurerm_resource_group.developer-rg.location
+  resource_group_name   = azurerm_resource_group.developer-rg.name
   size                  = var.vm_config.size
   admin_username        = var.vm_config.admin_username
-  network_interface_ids = [azurerm_network_interface.tf-practice-nic.id]
+  network_interface_ids = [azurerm_network_interface.developer-nic.id]
 
   custom_data = data.cloudinit_config.vm-init.rendered
 
@@ -137,10 +137,10 @@ resource "azurerm_linux_virtual_machine" "tf-practice-vm" {
   }
 
   source_image_reference {
-    publisher = data.azurerm_platform_image.tf-practice-image.publisher
-    offer     = data.azurerm_platform_image.tf-practice-image.offer
-    sku       = data.azurerm_platform_image.tf-practice-image.sku
-    version   = data.azurerm_platform_image.tf-practice-image.version
+    publisher = data.azurerm_platform_image.developer-vm-image.publisher
+    offer     = data.azurerm_platform_image.developer-vm-image.offer
+    sku       = data.azurerm_platform_image.developer-vm-image.sku
+    version   = data.azurerm_platform_image.developer-vm-image.version
   }
 
   tags = var.resource_group_config.tags
@@ -148,8 +148,12 @@ resource "azurerm_linux_virtual_machine" "tf-practice-vm" {
 #### Virtual Machine - End ####
 ### Virtual Machine - Resources - End ###
 
-data "azurerm_public_ip" "tf-practice-public-ip-data" {
-  name                = azurerm_public_ip.tf-practice-public-ip.name
-  resource_group_name = azurerm_resource_group.tf-practice-rg.name
-  depends_on          = [azurerm_linux_virtual_machine.tf-practice-vm]
+# This is a workaround to get the public IP address of the VM
+# as the public_ip_address is not available until the VM is created.
+# This is only necessary because I am using a Dynamic Public IP.
+# Without this, a `terraform refresh` would be needed to get the public IP.
+data "azurerm_public_ip" "developer-public-ip-data" {
+  name                = azurerm_public_ip.developer-public-ip.name
+  resource_group_name = azurerm_resource_group.developer-rg.name
+  depends_on          = [azurerm_linux_virtual_machine.developer-vm]
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "public_ip_address" {
-  value = data.azurerm_public_ip.tf-practice-public-ip-data.ip_address
-  # value = azurerm_public_ip.tf-practice-public-ip.ip_address
+  value = data.azurerm_public_ip.developer-public-ip-data.ip_address
+  # value = azurerm_public_ip.developer-public-ip.ip_address 
 }
